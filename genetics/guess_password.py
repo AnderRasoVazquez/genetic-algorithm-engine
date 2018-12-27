202fb93c0a8a1503829d8f67a1d8c64139051fc2
Ander Raso Vázquez <anderraso@gmail.com>
2018-12-27 16:20:33 +0100

    engine refactored, a.k.a the worlds longest hello world
    Signed-off-by: Ander Raso Vázquez <anderraso@gmail.com>

--- genetics/guess_password.py
+++ genetics/guess_password.py
@@ -1,60 +1,37 @@
-import random
 import datetime
-
-# genes to use for building guesses
-geneSet = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!."
-target = "Hello World!"
+import genetic
 
 
-
-def generate_parent(length):
-    """Generate a random string (with no duplicates) from the gene set."""
-    genes = []
-    while len(genes) < length:
-        sampleSize = min(length - len(genes), len(geneSet))
-        genes.extend(random.sample(geneSet, sampleSize))
-    return ''.join(genes)
+def get_fitness(genes, target):
+    """The only feedback the engine needs to evaluate the genes."""
+    return sum(1 for expected, actual in zip(target, genes) if expected == actual)
 
 
-def get_fitness(guess):
-    """The only feedback the engine needs to evaluate the guess."""
-    return sum(1 for expected, actual in zip(target, guess) if expected == actual)
-
-
-def mutate(parent):
-    """Mutate one letter from parent."""
-    index = random.randrange(0, len(parent))
-    childGenes = list(parent)
-    newGene, alternate = random.sample(geneSet, 2)
-    childGenes[index] = alternate if newGene == childGenes[index] else newGene
-    return "".join(childGenes)
-
-
-def display(guess, startTime):
+def display(genes, target, start_time):
     """Show output."""
-    timeDiff = datetime.datetime.now() - startTime
-    fitness = get_fitness(guess)
-    print("{0}\t{1}\t{2}".format(guess, fitness, str(timeDiff)))
+    time_diff = datetime.datetime.now() - start_time
+    fitness = get_fitness(genes, target)
+    print("{0}\t{1}\t{2}".format(genes, fitness, str(time_diff)))
 
 
-def main():
-    random.seed()
-    startTime = datetime.datetime.now()
-    bestParent = generate_parent(len(target))
-    bestFitness = get_fitness(bestParent)
-    display(bestParent, startTime)
+def guess_password(target):
+    gene_set = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!."
+    start_time = datetime.datetime.now()
 
-    while True:
-        child = mutate(bestParent)
-        childFitness = get_fitness(child)
-        if bestFitness >= childFitness:
-            continue
-        display(child, startTime)
-        if childFitness >= len(bestParent):
-            break
-        bestFitness = childFitness
-        bestParent = child
+    def fn_get_fitness(genes):
+        return get_fitness(genes, target)
+
+    def fn_display(genes):
+        display(genes, target, start_time)
+
+    optimal_fitness = len(target)
+    genetic.get_best(fn_get_fitness, len(target), optimal_fitness, gene_set, fn_display)
+
+
+def test_hello_world():
+    target = "Hello World!"
+    guess_password(target)
 
 
 if __name__ == '__main__':
-    main()
+    test_hello_world()
